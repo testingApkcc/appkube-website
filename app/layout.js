441524769737fe46b7132b76cc10d457dce526cc
Mustafa Mohammed <mustafa.mohammed@synectiks.com>
2023-10-07 12:32:42 +0530

    fonts family changed and added through css

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,8 @@
 import './globals.css'
 import '../styles/global.css'
 import '../styles/responsive.css'
-import { Inter } from 'next/font/google'
 import Nav from '../components/Nav';
 import Footer from '../components/Footer';
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -15,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
        <Nav/>
         {children}
         </body>
