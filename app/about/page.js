97de4a7110f44b6a45842e39b767e9424e6e3f2f
Mustafa Mohammed <mustafa.mohammed@synectiks.com>
2023-10-13 02:48:26 +0530

    appcentric images added

--- app/about/page.js
+++ app/about/page.js
@@ -41,21 +41,21 @@ const features = () => {
     </div>
     <div className="flex-col-bg">
           <div className="flex-col">
-            <div className="fimageblock"><img src="appcentric/cost optimization image p3.svg" alt="" /></div>
+            <div className="fimageblock"><img src="appcentric/cost optimization image p5.svg" alt="" /></div>
             <div className="ftextblock">
               <h4 className='section-title'>Navigating the Competitive Landscape</h4>
               <p>Brief about the challenges faced by most enterprises such as competition, the introduction of new products, and the struggle to deliver speed, agility, flexibility, and better customer experience due to Legacy Application, Infrastructure, and IT Processes.</p>
             </div>
           </div>
           <div className="flex-col">
-            <div className="fimageblock"><img src="appcentric/cost optimization image p4.svg" alt="" /></div>
+            <div className="fimageblock"><img src="appcentric/cost optimization image p6.svg" alt="" /></div>
             <div className="ftextblock flexorder pr">
               <h4 className='section-title'>Navigating the Competitive Landscape</h4>
               <p>Brief about the challenges faced by most enterprises such as competition, the introduction of new products, and the struggle to deliver speed, agility, flexibility, and better customer experience due to Legacy Application, Infrastructure, and IT Processes.</p>
             </div>
           </div>
           <div className="flex-col">
-            <div className="fimageblock"><img src="appcentric/cost optimization image p4.svg" alt="" /></div>
+            <div className="fimageblock"><img src="appcentric/cost optimization image p7.svg" alt="" /></div>
             <div className="ftextblock flexorder pr">
               <h4 className='section-title'>Navigating the Competitive Landscape</h4>
               <p>Brief about the challenges faced by most enterprises such as competition, the introduction of new products, and the struggle to deliver speed, agility, flexibility, and better customer experience due to Legacy Application, Infrastructure, and IT Processes.</p>
@@ -67,7 +67,7 @@ const features = () => {
             <div className="imageblock"><img src="appcentric/cost optimization image p8.svg" alt="" /></div>
             <div className="textblock pl pr">
               <h4 className='section-title maddy'>"Ready to revolutionize your cloud infrastructure?"</h4>
-              <a href="#" className='dev-btn'>explore appkube solutions</a>
+              <a href="#" className='dev-btn'><sup>*</sup>Get started with Appkube today!</a>
             </div>
           </div>
 
