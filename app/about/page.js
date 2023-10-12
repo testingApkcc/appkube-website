1359c6e66fc0cd18b31b83354652c171d469dbd3
Mustafa Mohammed <mustafa.mohammed@synectiks.com>
2023-10-13 04:01:58 +0530

    editing global.css

--- app/about/page.js
+++ app/about/page.js
@@ -3,7 +3,7 @@ import React from 'react'
 const features = () => {
   return (
   <>
-  <section id='devsecops'>
+  <section id='devsecops' className='container'>
     <div className="hero-appcentric">
      <div>
          <h1 className='title tl'>"Are Your Cloud Expenses <br />spiraling Out of Control?"</h1>
