27a04ef61cc3a235af91fd0d6e477591006f8844
Mustafa Mohammed <mustafa.mohammed@synectiks.com>
2023-10-20 16:54:27 +0530

    thank you page created

---
+++ app/thank/page.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const thankyou = () => {
+    return (
+        <section className='bg-grad'> <h1 className='subtitle mt-0 mb-0'>
+
+            Thank you for your submission!
+
+        </h1>
+            <h2 className='subtitle mt-0'>"We appreciate your interest and look forward to connecting with you at Gitex and beyond."</h2>
+        </section>
+    )
+}
+
+export default thankyou
