c39fe20e32ec5bd49fe97a2dc0e40da852c84ace
Mustafa Mohammed <mustafa.mohammed@synectiks.com>
2023-10-20 08:05:19 +0530

    styling contact form

--- app/contact/page.js
+++ app/contact/page.js
@@ -4,35 +4,41 @@ import React, { useState, useEffect } from 'react';
 const contact = () => {
     return (
         <>
-            <form action="" className='df fld gp jc-c ai-c'>
-                <div className='df'>
-                    <div >
-                        <label htmlFor="">First Name</label>
-                        <input type="text" />
+            <section>
+
+
+                <form action="" className='df fld gp jc-c'>
+                    <div className='df gp'>
+                        <div className='df fld'>
+                            <label htmlFor="">First Name</label>
+                            <input type="text" placeholder='First Name' />
+                        </div>
+                        <div className='df fld'>
+                            <label htmlFor="">Last Name</label>
+                            <input type="text" placeholder='Last Name' />
+                        </div>
+                    </div>
+                    <div className='form-field df fld'>
+                        <label htmlFor="">Email</label>
+                        <input type="email" placeholder='Enter Email' />
+                    </div>
+                    <div className='form-field df fld'>
+                        <label htmlFor="">Phone Number</label>
+                        <input type="tel" placeholder='Enter Phone Number' />
+                    </div>
+                    <div className='form-field df fld'>
+                        <label htmlFor="">Message</label>
+                        <textarea name="" id="" cols="30" rows="10" placeholder='Message'></textarea>
+                    </div>
+                    <div className='form-field df gp'>
+                        <input type="checkbox" name="" id="" />
+                        <small>You agree to our friendly <u> privacy policy</u></small>
                     </div>
                     <div>
-                        <label htmlFor="">Last Name</label>
-                        <input type="text" />
+                        <button type="submit" className="btn btn-primary">Send Message</button>
                     </div>
-                </div>
-                <div className='form-field'>
-                    <label htmlFor="">Email</label>
-                    <input type="email" />
-                </div>
-                <div className='form-field'>
-                    <label htmlFor="">Phone Number</label>
-                    <input type="tel" />
-                </div>
-                <div className='form-field'>
-                    <label htmlFor="">Message</label>
-                    <textarea name="" id="" cols="30" rows="10"></textarea>
-                </div>
-                <div className='form-field'>
-                    <input type="checkbox" name="" id="" />
-                    <small>You agree to our friendly <u> privacy policy</u></small>
-                </div>
-                <button type="submit" class="btn btn-primary">Send Message</button>
-            </form>
+                </form>
+            </section>
         </>
     )
 }
